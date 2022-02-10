403cd4f781bb9aa2911d9718448aad7ce5d34f48
BenHM10 <benhoffman.malau@gmail.com>
2022-02-10 21:04:50 +0700

    Create Tugas71190461.java
    71190461 program java

---
+++ Group B/71190461/Tugas71190461.java
@@ -0,0 +1,15 @@
+public class Tugas71190461 {
+    public static void main(String[] args) { 
+ 
+        int n = 461; 
+        int sum = 0; 
+
+        for (int i = 0; i <= n; i++) { 
+        	sum = sum + i; 
+       
+        } 
+
+    System.out.println(sum); 
+
+    } 
+}
