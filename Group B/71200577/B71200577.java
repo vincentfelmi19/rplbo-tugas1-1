2abe1b81836d3413ca6cfea1d5007635bbb83330
ignatius barry <ignatius.barry@ti.ukdw.ac.id>
2022-02-11 07:42:11 +0700

    71200577 program java

---
+++ Group B/71200577/B71200577.java
@@ -0,0 +1,27 @@
+package tugas1;
+
+public class B71200577 {
+		
+		  public static void main(String[] args) {
+			 
+			String txt = ("71200577 IGNATIUS BARRY SANTOSO");
+			
+		    int n;
+		    int tot = 0;
+		    
+		    n = 577;
+
+		    //a
+		    
+		    for(int i=1; i<=n; i++) {
+		      tot = tot + i;
+		    }
+
+		    System.out.println("3 DIGIT NIM : " + n + " TOTAL = " + tot + " " + txt);
+		    System.out.println("TERIMA KASIH");
+		    
+		  }
+		}
+	
+
+
