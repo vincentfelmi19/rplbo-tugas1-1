86051bf2f8887166a788b53176e687cbd65329ed
MaximHere <maximiliano.bagas@ti.ukdw.ac.id>
2022-02-11 07:14:03 +0700

    71200535 Program Java

---
+++ Group B/71200535/Tugas171200535.java
@@ -0,0 +1,19 @@
+
+public class Tugas171200535{
+
+	public static void main(String[] args) {
+		// NIM = 71200535
+		// 3 digit terakhir --> 535
+		int n = 535;
+		int total = 0;
+		
+		for(int i=1; i<=n; i++) {
+			
+            total = total + i;
+          }
+		
+		System.out.println(total);
+
+	}
+
+}
