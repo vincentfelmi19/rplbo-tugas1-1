c358639c422dcecf08415c390e3a26a9f09764f9
untungmangkunegara <untung.mangkunegara@ti.ukdw.ac.id>
2022-02-15 16:16:58 +0700

    Kumpul Ulang
    comit pertama

---
+++ Group D/71200619/71200619.java
@@ -0,0 +1,11 @@
+class sum { 
+    public static void main (String args[]){ 
+        int n = 619;  //3 angka NIM dari belakang NIM 
+        int sum = 0; 
+        for(int i=1;i<=n;i++) { 
+            sum = sum + i ;
+            }
+        System.out.println("Hasil Penjumlahan " + sum); 
+        System.out.println("TERIMA KASIH, 71200619 UNTUNG MANGKUNEGARA");
+        } 
+}
