8c30ceddf71ce642501814c56f3ba7fcfeb79a51
Putra Yehuda <putra.yehuda@ti.ukdw.ac.id>
2022-02-15 16:07:41 +0700

    Create 71200658.java
    71200658 Program Java

---
+++ Group D/71200658/71200658.java
@@ -0,0 +1,12 @@
+public class 71200658 {
+    public static void main(String args[]) {
+        int nim = 658;
+        int n = 0;
+
+        while(nim > 0){
+            n += nim;
+            nim -= 1;
+        }
+        System.out.println("Hasilnya adalah :" + n);
+    }
+}
