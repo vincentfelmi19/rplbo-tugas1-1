599f4206b5f2c38ba0b7fd759d2a24ac649c1777
rizkitateshalonica <rizkita.teshalonica@ti.ukdw.ac.id>
2022-02-15 16:12:33 +0700

    Create R71190497.java
    71190497 program java

---
+++ Group D/71190497/R71190497.java
@@ -0,0 +1,16 @@
+package rplbo;
+
+public class R71190497 {
+
+	public static void main(String[] args) {
+		int jum = 0;
+		int angka = 1;
+		int n = 497;
+
+        while (angka <= n){
+            jum += angka;
+            angka++;       
+        }
+       System.out.println("Hasil Penjumlahan= " + jum);
+	}
+}
