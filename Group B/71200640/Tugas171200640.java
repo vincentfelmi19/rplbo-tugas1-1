b73b1dde2580d6338e135df745d38db626bfac18
shalommita <78341414+shalommita@users.noreply.github.com>
2022-02-20 15:00:41 +0700

    Tugas1

---
+++ Group B/71200640/Tugas171200640.java
@@ -0,0 +1,14 @@
+public class Tugas171200640 {
+    public static void main(String[] args) {
+        int jml = 0;
+        int angka = 1;
+        int n = 640; // NIM 71200640 - 3 dgt 640
+
+        while (angka <= n){
+            jml += angka;
+            angka++;
+        }
+        System.out.println("Total= " + jml);
+        System.out.println("TERIMA KASIH - 71200640 Shalommita");
+    }
+}
